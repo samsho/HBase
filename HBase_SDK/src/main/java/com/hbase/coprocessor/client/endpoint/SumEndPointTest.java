6262f09bfaa8b943534f6396dacae97e633662fb
SamSho <shaomin_work@163.com>
2016-01-24 19:59:59 +0800

    endpoint

---
+++ HBase_SDK/src/main/java/com/hbase/coprocessor/client/endpoint/SumEndPointTest.java
@@ -0,0 +1,56 @@
+package com.hbase.coprocessor.client.endpoint;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HConnection;
+import org.apache.hadoop.hbase.client.HConnectionManager;
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.apache.hadoop.hbase.client.coprocessor.Batch;
+import org.apache.hadoop.hbase.ipc.BlockingRpcCallback;
+
+import java.io.IOException;
+
+/**
+ * Created by home on 2016/1/24.
+ */
+public class SumEndPointTest {
+
+public void test() throws IOException {
+
+
+//    Configuration conf = HBaseConfiguration.create();
+//    // Use below code for HBase version 1.x.x or above.
+//    Connection connection = ConnectionFactory.createConnection(conf);
+//    TableName tableName = TableName.valueOf("users");
+//    Table table = connection.getTable(tableName);
+
+
+    //Use below code HBase version 0.98.xx or below.
+    HConnection connection = HConnectionManager.createConnection(conf);
+    HTableInterface table = connection.getTable("users");
+
+    final SumRequest request = SumRequest.newBuilder().setFamily("salaryDet").setColumn("gross")
+            .build();
+    try {
+        Map<byte[], Long> results = table.CoprocessorService (SumService.class, null, null,
+                new Batch.Call<SumService, Long>() {
+                    @Override
+                    public Long call(SumService aggregate) throws IOException {
+                        BlockingRpcCallback rpcCallback = new BlockingRpcCallback();
+                        aggregate.getSum(null, request, rpcCallback);
+                        SumResponse response = rpcCallback.get();
+                        return response.hasSum() ? response.getSum() : 0L;
+                    }
+                });
+        for (Long sum : results.values()) {
+            System.out.println("Sum = " + sum);
+        }
+    } catch (ServiceException e) {
+        e.printStackTrace();
+    } catch (Throwable e) {
+        e.printStackTrace();
+    }
+}
+
+}
--- HBase_SDK/src/main/java/com/hbase/coprocessor/server/observer/RegionObserverExample.java
+++ HBase_SDK/src/main/java/com/hbase/coprocessor/server/observer/RegionObserverExample.java
@@ -7,6 +7,9 @@ import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.regionserver.FlushRequester;
+import org.apache.hadoop.hbase.regionserver.HRegion;
+import org.apache.hadoop.hbase.regionserver.RegionServerServices;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
@@ -33,6 +36,26 @@ public class RegionObserverExample extends BaseRegionObserver{
             KeyValue kv = new KeyValue(get.getRow(), FIXED_ROW, FIXED_ROW, Bytes.toBytes(System.currentTimeMillis()));
             LOG.debug("Had a match, adding fake kv: " + kv);
             result.add(kv);
+
+//            c.bypass();//一旦特殊的keyValue被添加，之后的操作都会被跳过。
         }
+    }
+
+    @Override
+    public void preOpen(ObserverContext<RegionCoprocessorEnvironment> e) throws IOException {
+
+        RegionCoprocessorEnvironment env = e.getEnvironment();
+        HRegion hRegion = env.getRegion();
+
+        RegionServerServices regionServerServices = env.getRegionServerServices();
+        FlushRequester flushRequester = regionServerServices.getFlushRequester();
+        flushRequester.requestFlush(hRegion);
+
+
+    }
+
+    @Override
+    public void preSplit(ObserverContext<RegionCoprocessorEnvironment> e) throws IOException {
+        e.bypass();
     }
 }
